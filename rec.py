51a47ed9c16ae004185959f81268028e887f8168
zhangrh93 <30519667+zhangrh93@users.noreply.github.com>
2018-08-27 01:54:40 +1000

    update for generated data

--- rec.py
+++ rec.py
@@ -26,40 +26,23 @@ with open("train.txt") as trainfile:
         train[a] = list(set(train[a]))
 
         
-
-test = []
-
-with open("test-public.txt") as testfile:
-    for i, line in enumerate(testfile):
-        if i == 0:
-            continue;
-        line_list = [int(k) for k in line[:-1].split("\t")];
-        test.append([line_list[1],line_list[2]])
-
 print ("--------complete")
 print ("generating dictionary")
-        
-vector_count = {}
 
-for i in train:
-    for j in train[i]:
-        vector_count[j] = vector_count.get(j,0)+1
 
+#generate new node set
 new_set = set()
 for i in vector_count:
     if vector_count[i]>threshold:
         new_set.add(i)
 
-for i,j in test:
-    new_set.add(j)
-
 for i in train:
     new_set.add(i)
 
 with open("raw.pickle","rb") as f:
-    ttrain = pickle.load(f)
+    test = pickle.load(f)
     
-for i,j,k,_ in ttrain:
+for i,j,k,_ in test:
     new_set.add(j)
     new_set.add(k)
     
@@ -71,52 +54,39 @@ for i,j in enumerate(id2v):
 print ("length of new set:")
 print (len(new_set))
 
+#generate new node id dictionary
 new_train = {}
 for i in train:
-    new_train[v2id[i]] = [v2id[j] for j in train[i] if j in new_set]
+    new_train[v2id[i]] = set([v2id[j] for j in train[i] if j in new_set])
+    
+
     
 new_test = {}
-for i,j,k,_ in ttrain:
+for i,j,k,_ in test:
     new_test[i] = [v2id[j],v2id[k]]
+    if v2id[k] in new_train[v2id[j]]:
+        new_train[v2id[j]].remove(v2id[k])
+    
     
 tA = new_train.copy()
 tB = {}
 for i in new_train:
     if i not in tA[i]:
-        tA[i].append(i)
+        tA[i].add(i)
     for j in new_train[i]:
-        tB[j] = tB.get(j,[])
-        tB[j].append(i)
+        tB[j] = tB.get(j,set([]))
+        tB[j].add(i)
 
 print ("now processing...")
 
-
-def get_feature(i,tA,l,layer=1):
-    wlist = [[i,1]]
-    temp = np.zeros(l)
-    for i in range(layer):
-        tlist = []
-        for ni,scale in wlist:
-            if ni in tA:
-                nscale = scale/len(tA[ni])
-                temp[tA[ni]] +=nscale
-                if i < (layer-1):
-                    for nj in tA[ni]:
-                        tlist.append([nj,nscale])
-            else:
-                temp[ni]+=scale
-                tlist.append([ni,scale])
-            #print (1)
-        wlist = tlist
-    return np.array(temp)
 
 def sim(pair,tA,tB,l):
     vi,vj = pair
     tempA = np.zeros(l);
     tempB = np.zeros(l);
-    tempA[tA[vi]] = 1/len(tA[vi]);
+    tempA[list(tA[vi])] = 1/len(tA[vi]);
     for i in tB[vj]:
-        tempB[tA[i]] += 1/len(tB[vj])/len(tA[i]);
+        tempB[list(tA[i])] += 1/len(tB[vj])/len(tA[i]);
     return cosine_similarity([tempA, tempB])[0][1]
     #return tempA,tempB
 
